6fcd28c7c1b0f30c6d1930283e57ad8f4bb34adc
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-09-15 11:02:35 +0600

    added Course title

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+      return (
+            <h1 className='text-[#1C1B1B] text-5xl py-8 text-center font-semibold'>Course Registration</h1>
+      );
+};
+
+export default Header;
